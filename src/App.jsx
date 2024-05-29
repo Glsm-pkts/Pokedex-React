6cd39f447006c3449087149fd657559a66fa4d96
Glsm-pkts <gulsumpektass@gmail.com>
2024-05-29 16:20:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import './Poke.css';
+import Pokedex from './Pokedex';
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Pokemon Go Card Game</h1>
+      
+        <Pokedex />
+      
+      
+    </>
+  )
+}
+
+export default App
